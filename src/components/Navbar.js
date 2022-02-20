87e11e077bb675edeb91792753bceabed4a25fe5
Ric Taglietti <rictaglietti@gmail.com>
2022-02-20 12:41:12 -0500

    add removeCookie var to useCookie hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { useCookies } from 'react-cookie';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [cookies, removeCookie] = useCookies(['user']);
+  const [cookies, setCookie, removeCookie] = useCookies(['user']);
 
   const handleLogout = () => {
     removeCookie('user', { path: '/' });
