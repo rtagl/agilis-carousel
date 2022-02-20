c1005262e4d44ffad956f39672fbdf9232a9165e
Ric Taglietti <rictaglietti@gmail.com>
2022-02-19 19:25:29 -0500

    change displayed carousel images based on user cookie

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import { StyledCarousel } from './styles/StyledCarousel.styled';
 import { loggedOutImages, loggedInImages } from '../images';
 import CarouselControls from './CarouselControls';
 
 const Carousel = () => {
+  const [cookie] = useCookies();
   const [images, setImages] = useState(loggedOutImages);
   const [selectedImage, setSelectedImage] = useState(0);
+
+  useEffect(() => {
+    console.log(cookie['user'], '4389483284');
+    if (cookie['user']) {
+      setImages(loggedInImages);
+    } else {
+      setImages(loggedOutImages);
+    }
+  }, [cookie]);
+
   return (
     <StyledCarousel>
       <div className="images">
--- src/images.js
+++ src/images.js
@@ -24,7 +24,7 @@ export const loggedInImages = [
     url: 'https://images.unsplash.com/photo-1492937633366-f60df9267ba6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2lucXVlJTIwdGVycmV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
   },
   {
-    url: 'https://images.unsplash.com/photo-1604430466284-e3a33823187e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9sb21pdGVzfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=400&q=60',
+    url: 'https://images.unsplash.com/photo-1632316594232-663ccefa8fdd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXRhbGlhbiUyMHZpbGxhZ2V8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
   },
   {
     url: 'https://images.unsplash.com/photo-1603531591088-2334d0350c48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2lucXVlJTIwdGVycmV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
